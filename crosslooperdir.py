08e4b165cd0b6ea23502a599c170b5df81cdce12
Splendide Imaginarius <119545140+Splendide-Imaginarius@users.noreply.github.com>
2023-02-13 00:02:42 +0000

    Add multiprocessing to crosslooperdir

--- crosslooperdir.py
+++ crosslooperdir.py
@@ -1,5 +1,7 @@
 #!/usr/bin/env python3
 
+from multiprocessing import Process, Queue, Lock
+import os
 from pathlib import Path
 
 from tqdm import tqdm
@@ -8,6 +10,19 @@ import crosslooper
 
 __version__ = crosslooper.__version__
 __author__ = crosslooper.__author__
+
+def loop_process_run(input_file_queue, progress_queue, pbar_lock, process_num, ka):
+  tqdm.set_lock(pbar_lock)
+  single_pbar = tqdm(unit='audio_sec', position=process_num+1)
+
+  while(True):
+    finished, f = input_file_queue.get()
+    if finished:
+      break
+
+    crosslooper.file_offset(in1=f, pbar=single_pbar, **ka)
+
+    progress_queue.put(1)
 
 def file_offset_dir(path, **ka):
   """CLI interface to save loop metadata of a directory of audio files.
@@ -18,15 +33,35 @@ def file_offset_dir(path, **ka):
 
   files = list(path.glob(f"**/*"))
 
-  total_pbar = tqdm(unit='track')
+  pbar_lock = Lock()
+  tqdm.set_lock(pbar_lock)
+
+  total_pbar = tqdm(unit='track', position=0)
   total_pbar.set_description('folder')
   total_pbar.reset(total=len(files))
 
-  single_pbar = tqdm(unit='audio_sec')
+  input_file_queue = Queue()
+
+  process_num = os.cpu_count()
+
+  progress_queue = Queue()
+
+  loop_processes = []
+  for p in range(process_num):
+    loop_processes.append(Process(target=loop_process_run, args=(input_file_queue, progress_queue, pbar_lock, p, ka)))
+
+  for p in loop_processes:
+    p.start()
 
   for f in files:
-    crosslooper.file_offset(in1=f, pbar=single_pbar, **ka)
+    input_file_queue.put((False, f))
+
+  for f in files:
+    progress_queue.get()
     total_pbar.update(1)
+
+  for p in loop_processes:
+    input_file_queue.put((True, None))
 
 main = file_offset_dir
 if __name__ == '__main__':
